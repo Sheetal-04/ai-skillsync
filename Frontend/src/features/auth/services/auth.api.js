f6b7411608e7b7625d4a4d1cc065598abbe47827
Sheetal <sheetaltyagichd@gmail.com>
2026-06-16 18:03:25 +0530

    Fix:fixed bugs

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000" || "https://aiskillsync-api.codingprod.live" || "https://ai-skillsync-k2a1.onrender.com",
     withCredentials: true
 })
 
