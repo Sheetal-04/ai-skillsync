0037823d7a73870fe960eabf99938a73fe3a626d
Sheetal <sheetaltyagichd@gmail.com>
2026-06-16 18:14:19 +0530

    Fix:fixed client url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000" || "https://aiskillsync-api.codingprod.live" || "https://ai-skillsync-k2a1.onrender.com",
+    baseURL: "https://aiskillsync-api.codingprod.live" || "https://ai-skillsync-k2a1.onrender.com",
+    // baseURL: "http://localhost:3000" || "https://aiskillsync-api.codingprod.live" || "https://ai-skillsync-k2a1.onrender.com",
     withCredentials: true
 })
 
